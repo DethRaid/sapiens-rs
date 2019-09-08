28a66c8d877764c347f444d5d02e89460a17017b
David Dubois <dexcelstraun7@gmail.com>
2019-09-08 12:31:06 -0700

    Greatly expanded the math wrapper

--- src/sp/math.rs
+++ src/sp/math.rs
@@ -1,15 +1,21 @@
 use sapiens_sys::*;
-use std::ops::{Add, Div, Mul, Neg, Sub};
+use std::ops::{Add, Div, Index, IndexMut, Mul, Neg, Sub};
 
 #[derive(Debug)]
 pub struct Vec2(SPVec2);
 
+#[derive(Debug)]
 pub struct Vec3(SPVec3);
 
+#[derive(Debug)]
 pub struct Vec4(SPVec4);
 
+pub type Quat = Vec4;
+
+#[derive(Debug)]
 pub struct Mat3(SPMat3);
 
+#[derive(Debug)]
 pub struct Mat4(SPMat4);
 
 impl Vec2 {
@@ -19,6 +25,12 @@ impl Vec2 {
 
     pub fn as_sp_vec(&self) -> SPVec2 {
         self.0
+    }
+}
+
+impl Default for Vec2 {
+    fn default() -> Self {
+        Vec2::new(0.0, 0.0)
     }
 }
 
@@ -63,6 +75,10 @@ impl Neg for Vec2 {
 }
 
 impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3(SPVec3 { x, y, z })
+    }
+
     /// Normalizes the provided vector
     pub fn normalize(vec: &Vec3) -> Vec3 {
         Vec3(unsafe { spVec3Normalize(vec.0) })
@@ -102,8 +118,19 @@ impl Vec3 {
         unsafe { spVec3Length2(self.0) }
     }
 
+    /// Checks if this Vec3 is to the left of the line from a to b
+    pub fn is_left_of_line(&self, a: &Vec3, b: &Vec3) -> bool {
+        unsafe { spPointIsLeftOfLine(self.0, a.0, b.0) }
+    }
+
     pub fn as_sp_vec(&self) -> SPVec3 {
         self.0
+    }
+}
+
+impl Default for Vec3 {
+    fn default() -> Self {
+        Vec3::new(0.0, 0.0, 0.0)
     }
 }
 
@@ -156,8 +183,18 @@ impl Neg for Vec3 {
 }
 
 impl Vec4 {
+    pub fn new(x: f64, y: f64, z: f64, w: f64) -> Self {
+        Vec4(SPVec4 { x, y, z, w })
+    }
+
     pub fn as_sp_vec(&self) -> SPVec4 {
         self.0
+    }
+}
+
+impl Default for Vec4 {
+    fn default() -> Self {
+        Vec4::new(0.0, 0.0, 0.0, 0.0)
     }
 }
 
@@ -201,15 +238,140 @@ impl Neg for Vec4 {
     }
 }
 
+impl Quat {
+    /// Spherical linear interpolation between two quaternions
+    ///
+    /// aka interpolates between two rotations
+    pub fn slerp(a: &Quat, b: &Quat, x: f64) -> Quat {
+        Vec4(unsafe { spQuatSlerp(a.0, b.0, a) })
+    }
+}
+
 impl Mat3 {
+    /// Constructs a new Mat4 from the provided rows
+    ///
+    /// TODO: Verify that this is what Sapiens expects
+    pub fn new(row0: &Vec3, row1: &Vec3, row2: &Vec3) -> Self {
+        Mat3(unsafe { spMat3FromVec3s(row0.0, row1.0, row2.0) })
+    }
+
+    pub fn identity() -> Self {
+        Mat3(unsafe { spMat3Identity() })
+    }
+
+    pub fn look_at_inverse(look: &Vec3, up: &Vec3) -> Mat3 {
+        Mat3(unsafe { spMat3LookAtInverse(look.0, up.0) })
+    }
+
+    /// Spherical interpolation between two Mat3
+    ///
+    /// AKA interpolates between two rotation matrices
+    pub fn slerp(lhs: &Mat3, rhs: &Mat3, fraction: f64) -> Mat3 {
+        Mat3(unsafe { spMat3Slerp(lhs.0, rhs.0, fraction) })
+    }
+
     pub fn as_sp_mat(&self) -> SPMat3 {
         self.0
+    }
+
+    /// Rotates this matrix by some amount around an axis, and returns the result
+    ///
+    /// This method doesn't modify the matrix itself
+    pub fn rotate(&self, angle: f64, axis: &Vec3) -> Mat3 {
+        Mat3(unsafe { spMat3Rotate(self.0, angle, axis.0) })
+    }
+
+    /// calculates the inverse of this matrix
+    pub fn inverse(&self) -> Mat3 {
+        Mat3(unsafe { spMat3Inverse(self.0) })
+    }
+}
+
+impl Default for Mat3 {
+    /// Creates an 3x3 identity matrix
+    fn default() -> Self {
+        Mat3::identity()
+    }
+}
+
+impl From<Quat> for Mat3 {
+    fn from(quat: Quat) -> Self {
+        let mut mat: Mat3 = Default::default();
+
+        unsafe { spMat3Cast(&mut quat.0, &mut mat.0) }
+
+        mat
+    }
+}
+
+impl Into<Quat> for Mat3 {
+    fn into(self) -> Quat {
+        Vec4(unsafe { spQuatCast(&mut self.0) })
+    }
+}
+
+impl Index<u32> for Mat3 {
+    type Output = Vec3;
+
+    /// Gets the row at the specified index
+    fn index(&self, index: u32) -> &Self::Output {
+        &Vec3(unsafe { spMat3GetRow(self.0, index as i32) })
+    }
+}
+
+impl IndexMut<u32> for Mat3 {
+    fn index_mut(&mut self, index: u32) -> &mut Self::Output {
+        &mut Vec3(unsafe { spMat3GetRow(self.0, index as i32) })
+    }
+}
+
+impl Mul for Mat3 {
+    type Output = Mat3;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Mat3(unsafe { spMat3Multiply(self.0, rhs.0) })
     }
 }
 
 impl Mat4 {
+    /// Constructs a new Mat4 from the provided rows
+    ///
+    /// TODO: Verify that this is what Sapiens expects
+    pub fn new(row0: &Vec4, row1: &Vec4, row2: &Vec4, row3: &Vec4) -> Self {
+        Mat4(SPMat4 {
+            m0: row0.0.x,
+            m1: row0.0.y,
+            m2: row0.0.z,
+            m3: row0.0.w,
+            m4: row1.0.x,
+            m5: row1.0.y,
+            m6: row1.0.z,
+            m7: row1.0.w,
+            m8: row2.0.x,
+            m9: row2.0.y,
+            m10: row2.0.z,
+            m11: row2.0.w,
+            m12: row3.0.x,
+            m13: row3.0.y,
+            m14: row3.0.z,
+            m15: row3.0.w,
+        })
+    }
+
     pub fn as_sp_mat(&self) -> SPMat4 {
         self.0
+    }
+}
+
+impl Default for Mat4 {
+    /// Creates a 4x4 identity matrix
+    fn default() -> Self {
+        Mat4::new(
+            &Vec4::new(1.0, 0.0, 0.0, 0.0),
+            &Vec4::new(0.0, 1.0, 0.0, 0.0),
+            &Vec4::new(0.0, 0.0, 1.0, 0.0),
+            &Vec4::new(0.0, 0.0, 0.0, 1.0),
+        )
     }
 }
 
