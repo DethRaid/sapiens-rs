ea524ef0a43ed3c41a71c32e00ee95a4ffa002f2
David Dubois <dexcelstraun7@gmail.com>
2019-09-09 22:40:15 -0700

    Example mod compiles amazingly

--- examples/more-smoke-mod.rs
+++ examples/more-smoke-mod.rs
@@ -1,15 +1,17 @@
 extern crate sapiens_rs;
 extern crate sapiens_sys;
 
+extern crate num_derive;
+
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
 use sapiens_sys::*;
 
-use sapiens_rs::sp::math::Vec3;
-use sapiens_rs::sp::{vec3_mul, vec3_add};
+use sapiens_rs::sp::{vec3_add, vec3_mul};
 
 /// Sapiens calls this function once per particle to update that particle's position, velocity, and rendering
 /// information
-///
-/// Rather than
 #[allow(non_snake_case)]
 extern "C" fn spUpdateParticle(
     thread_state: *mut SPParticleThreadState,
@@ -29,13 +31,17 @@ extern "C" fn spUpdateParticle(
     )
 }
 
+/// All the types of emitters that your particles mod supports
+#[derive(FromPrimitive)]
 enum EmitterType {
     Campfire = 1,
     WoodChop = 2,
     Feathers = 3,
 }
 
+/// ALl the different ways your mod can render a particle
 #[repr(u32)]
+#[derive(FromPrimitive)]
 enum ParticleRenderType {
     Smoke = 1,
     Fire = 2,
@@ -52,10 +58,10 @@ pub fn update_particle(
     origin: SPVec3,
     render_buffer: *mut f32,
 ) -> bool {
-    let life_left_multiplier = match render_group {
-        ParticleRenderType::Smoke as u32 => 0.01,
-        ParticleRenderType::Fire as u32 => 1.5 - particle_state.randomValueB * 0.5,
-        ParticleRenderType::Spark as u32 => 1.5 - particle_state.randomValueB * 1.0,
+    let life_left_multiplier = match FromPrimitive::from_u32(render_group) {
+        Some(ParticleRenderType::Smoke) => 0.01,
+        Some(ParticleRenderType::Fire) => 1.5 - particle_state.randomValueB * 0.5,
+        Some(ParticleRenderType::Spark) => 1.5 - particle_state.randomValueB * 1.0,
         _ => 1.0,
     };
 
@@ -66,8 +72,8 @@ pub fn update_particle(
         return false;
     }
 
-    match render_group as ParticleRenderType {
-        ParticleRenderType::Smoke => {
+    match FromPrimitive::from_u32(render_group) {
+        Some(ParticleRenderType::Smoke) => {
             particle_state.v = vec3_mul(particle_state.v, 1.0 - delta_time * 0.05);
 
             let vel = vec3_add(particle_state.v, particle_state.gravity);
@@ -76,11 +82,14 @@ pub fn update_particle(
             particle_state.scale = particle_state.scale
                 + delta_time * particle_state.lifeLeft * (1.0 + particle_state.randomValueA) * 0.15;
         }
-        ParticleRenderType::Fire => {
-            particle_state.p = vec3_mul(particle_state.p,2.0 - life_left) * delta_time);
+        Some(ParticleRenderType::Fire) => {
+            particle_state.p = vec3_mul(particle_state.p, 2.0 - life_left * delta_time);
         }
         _ => {
-            particle_state.v = vec3_add(particle_state.v,vec3_mul(particle_state.gravity, delta_time));
+            particle_state.v = vec3_add(
+                particle_state.v,
+                vec3_mul(particle_state.gravity, delta_time),
+            );
             particle_state.p = vec3_add(particle_state.p, vec3_mul(particle_state.v, delta_time));
         }
     }
