980955e9d707d6cad9f754bd9aa8d38004d74603
David Dubois <dexcelstraun7@gmail.com>
2019-09-10 08:56:55 -0700

    Restore cargo toml after bad commit

--- sapiens-sys/resources/wrapper.h
+++ sapiens-sys/resources/wrapper.h
@@ -4,12 +4,12 @@
  */
 
 // Include Sapiens headers so bindgen has everything available
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPBiome.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPClimate.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPCommon.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPHeight.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPMath.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPNoise.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPParticles.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPRainfall.h"
-#include "../extern/sapiens-mod-creation/SPlugins/include/SPRand.h"
+#include <SPBiome.h>
+#include <SPClimate.h>
+#include <SPCommon.h>
+#include <SPHeight.h>
+#include <SPMath.h>
+#include <SPNoise.h>
+#include <SPParticles.h>
+#include <SPRainfall.h>
+#include <SPRand.h>
