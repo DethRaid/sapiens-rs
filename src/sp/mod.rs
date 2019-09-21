8a474e57885137ee99aa27c362804cf87de80da4
David Dubois <dexcelstraun7@gmail.com>
2019-09-21 15:50:28 -0700

    Life is a borrow :(

--- src/sp/mod.rs
+++ src/sp/mod.rs
@@ -47,122 +47,122 @@ pub fn smooth_step(edge0: f64, edge1: f64, x: f64) -> f64 {
 }
 
 /// Adds two vectors together
-pub fn vec2_add(a: SPVec2, b: SPVec2) -> SPVec2 {
-    unsafe { spVec2Add(a, b) }
+pub fn vec2_add(a: &SPVec2, b: &SPVec2) -> SPVec2 {
+    unsafe { spVec2Add(*a, *b) }
 }
 
 /// Subtracts b from a
-pub fn vec2_sub(a: SPVec2, b: SPVec2) -> SPVec2 {
-    unsafe { spVec2Sub(a, b) }
+pub fn vec2_sub(a: &SPVec2, b: &SPVec2) -> SPVec2 {
+    unsafe { spVec2Sub(*a, *b) }
 }
 
 /// Multiplies a by b
-pub fn vec2_mul(a: SPVec2, b: f64) -> SPVec2 {
-    unsafe { spVec2Mul(a, b) }
+pub fn vec2_mul(a: &SPVec2, b: f64) -> SPVec2 {
+    unsafe { spVec2Mul(*a, b) }
 }
 
 /// Divides a by b
-pub fn vec2_div(a: SPVec2, b: f64) -> SPVec2 {
-    unsafe { spVec2Div(a, b) }
+pub fn vec2_div(a: &SPVec2, b: f64) -> SPVec2 {
+    unsafe { spVec2Div(*a, b) }
 }
 
 /// Negates a
-pub fn vec2_neg(a: SPVec2) -> SPVec2 {
-    unsafe { spVec2Neg(a) }
+pub fn vec2_neg(a: &SPVec2) -> SPVec2 {
+    unsafe { spVec2Neg(*a) }
 }
 
 /// Adds a to be
-pub fn vec3_add(a: SPVec3, b: SPVec3) -> SPVec3 {
-    unsafe { spVec3Add(a, b) }
+pub fn vec3_add(a: &SPVec3, b: &SPVec3) -> SPVec3 {
+    unsafe { spVec3Add(*a, *b) }
 }
 
 /// Subtracts b from a
-pub fn vec3_sub(a: SPVec3, b: SPVec3) -> SPVec3 {
-    unsafe { spVec3Sub(a, b) }
+pub fn vec3_sub(a: &SPVec3, b: &SPVec3) -> SPVec3 {
+    unsafe { spVec3Sub(*a, *b) }
 }
 
 /// Multiplies a by b
-pub fn vec3_mul(a: SPVec3, b: f64) -> SPVec3 {
-    unsafe { spVec3Mul(a, b) }
+pub fn vec3_mul(a: &SPVec3, b: f64) -> SPVec3 {
+    unsafe { spVec3Mul(*a, b) }
 }
 
 /// Divides a by b
-pub fn vec3_div(a: SPVec3, b: f64) -> SPVec3 {
-    unsafe { spVec3Div(a, b) }
+pub fn vec3_div(a: &SPVec3, b: f64) -> SPVec3 {
+    unsafe { spVec3Div(*a, b) }
 }
 
 /// Negates a
-pub fn vec3_neg(a: SPVec3) -> SPVec3 {
-    unsafe { spVec3Neg(a) }
+pub fn vec3_neg(a: &SPVec3) -> SPVec3 {
+    unsafe { spVec3Neg(*a) }
 }
 
 /// Adds a to b
-pub fn vec4_add(a: SPVec4, b: SPVec4) -> SPVec4 {
-    unsafe { spVec4Add(a, b) }
+pub fn vec4_add(a: &SPVec4, b: &SPVec4) -> SPVec4 {
+    unsafe { spVec4Add(*a, *b) }
 }
 
 /// Subtracts b from a
-pub fn vec4_sub(a: SPVec4, b: SPVec4) -> SPVec4 {
-    unsafe { spVec4Sub(a, b) }
+pub fn vec4_sub(a: &SPVec4, b: &SPVec4) -> SPVec4 {
+    unsafe { spVec4Sub(*a, *b) }
 }
 
 /// Multiplies a by b
-pub fn vec4_mul(a: SPVec4, b: f64) -> SPVec4 {
-    unsafe { spVec4Mul(a, b) }
+pub fn vec4_mul(a: &SPVec4, b: f64) -> SPVec4 {
+    unsafe { spVec4Mul(*a, b) }
 }
 
 /// Divides every component of a by b
-pub fn vec4_div(a: SPVec4, b: f64) -> SPVec4 {
-    unsafe { spVec4Div(a, b) }
+pub fn vec4_div(a: &SPVec4, b: f64) -> SPVec4 {
+    unsafe { spVec4Div(*a, b) }
 }
 
 /// Negates a
-pub fn vec4_neg(a: SPVec4) -> SPVec4 {
-    unsafe { spVec4Neg(a) }
+pub fn vec4_neg(a: &SPVec4) -> SPVec4 {
+    unsafe { spVec4Neg(*a) }
 }
 
 /// Normalizes a
-pub fn vec3_normalize(a: SPVec3) -> SPVec3 {
-    unsafe { spVec3Normalize(a) }
+pub fn vec3_normalize(a: &SPVec3) -> SPVec3 {
+    unsafe { spVec3Normalize(*a) }
 }
 
 /// Calculates the dot product of a and b
-pub fn vec3_dot(a: SPVec3, b: SPVec3) -> f64 {
-    unsafe { spVec3Dot(a, b) }
+pub fn vec3_dot(a: &SPVec3, b: &SPVec3) -> f64 {
+    unsafe { spVec3Dot(*a, *b) }
 }
 
 /// Calculates the cross product of a and b
-pub fn vec3_cross(a: SPVec3, b: SPVec3) -> SPVec3 {
-    unsafe { spVec3Cross(a, b) }
+pub fn vec3_cross(a: &SPVec3, b: &SPVec3) -> SPVec3 {
+    unsafe { spVec3Cross(*a, *b) }
 }
 
 /// Calculates the length of a vector
-pub fn vec3_length(v: SPVec3) -> f64 {
-    unsafe { spVec3Length(v) }
+pub fn vec3_length(v: &SPVec3) -> f64 {
+    unsafe { spVec3Length(*v) }
 }
 
 /// Calculates the squared length of a vector
 ///
 /// This function avoids a square root and thus is faster in many algorithms
-pub fn vec3_length2(v: SPVec3) -> f64 {
-    unsafe { spVec3Length2(v) }
+pub fn vec3_length2(v: &SPVec3) -> f64 {
+    unsafe { spVec3Length2(*v) }
 }
 
 /// Calculates the distance between two points
-pub fn vec3_distance(a: SPVec3, b: SPVec3) -> f64 {
-    unsafe { spVec3Distance(a, b) }
+pub fn vec3_distance(a: &SPVec3, b: &SPVec3) -> f64 {
+    unsafe { spVec3Distance(*a, *b) }
 }
 
 /// Calculates the squared distance between two points
 ///
 /// This function avoids a square root, and thus is faster in many algorithms
-pub fn vec3_distance2(a: SPVec3, b: SPVec3) -> f64 {
-    unsafe { spVec3Distance2(a, b) }
+pub fn vec3_distance2(a: &SPVec3, b: &SPVec3) -> f64 {
+    unsafe { spVec3Distance2(*a, *b) }
 }
 
 /// Multiplies v by m
-pub fn vec3_x_mat3(v: SPVec3, m: SPMat3) -> SPVec3 {
-    unsafe { spVec3xMat3(v, m) }
+pub fn vec3_x_mat3(v: &SPVec3, m: &SPMat3) -> SPVec3 {
+    unsafe { spVec3xMat3(*v, *m) }
 }
 
 /// Casts a 3x3 matrix to a quaternion
@@ -179,8 +179,8 @@ pub fn mat3_cast(q: *mut SPQuat, m: *mut SPMat3) {
 
 /// Spherically interpolates between two quaternions, effectively producing a rotation some percentage of the way
 /// between a and b
-pub fn quat_slerp(a: SPQuat, b: SPQuat, x: f64) -> SPQuat {
-    unsafe { spQuatSlerp(a, b, x) }
+pub fn quat_slerp(a: &SPQuat, b: &SPQuat, x: f64) -> SPQuat {
+    unsafe { spQuatSlerp(*a, *b, x) }
 }
 
 /// Gets a 3x3 identity matrix
@@ -189,39 +189,39 @@ pub fn mat3_identity() -> SPMat3 {
 }
 
 /// Encodes a rotate of `angle` (degrees or radians) around axis `axis` into matrix `m`
-pub fn mat3_rotate(m: SPMat3, angle: f64, axis: SPVec3) -> SPMat3 {
-    unsafe { spMat3Rotate(m, angle, axis) }
+pub fn mat3_rotate(m: &SPMat3, angle: f64, axis: &SPVec3) -> SPMat3 {
+    unsafe { spMat3Rotate(*m, angle, *axis) }
 }
 
 /// Calculates the inverse of matrix `m`
-pub fn mat3_inverse(m: SPMat3) -> SPMat3 {
-    unsafe { spMat3Inverse(m) }
+pub fn mat3_inverse(m: &SPMat3) -> SPMat3 {
+    unsafe { spMat3Inverse(*m) }
 }
 
 /// Multiplies a by b
-pub fn mat3_multiply(a: SPMat3, b: SPMat3) -> SPMat3 {
-    unsafe { spMat3Multiply(a, b) }
+pub fn mat3_multiply(a: &SPMat3, b: &SPMat3) -> SPMat3 {
+    unsafe { spMat3Multiply(*a, *b) }
 }
 
 /// Spherically interpolates between a and b, effectively returning a rotation that's `fraction` percent of the way
 /// between `a` and `b`
-pub fn mat3_slerp(a: SPMat3, b: SPMat3, fraction: f64) -> SPMat3 {
-    unsafe { spMat3Slerp(a, b, fraction) }
+pub fn mat3_slerp(a: &SPMat3, b: &SPMat3, fraction: f64) -> SPMat3 {
+    unsafe { spMat3Slerp(*a, *b, fraction) }
 }
 
 /// Retrieves a row from a matrix
-pub fn mat3_get_row(m: SPMat3, row_index: i32) -> SPVec3 {
-    unsafe { spMat3GetRow(m, row_index) }
+pub fn mat3_get_row(m: &SPMat3, row_index: i32) -> SPVec3 {
+    unsafe { spMat3GetRow(*m, row_index) }
 }
 
 /// Constructs a 3x3 matrix from its rows
-pub fn mat3_from_vec3s(a: SPVec3, b: SPVec3, c: SPVec3) -> SPMat3 {
-    unsafe { spMat3FromVec3s(a, b, c) }
+pub fn mat3_from_vec3s(a: &SPVec3, b: &SPVec3, c: SPVec3) -> SPMat3 {
+    unsafe { spMat3FromVec3s(*a, *b, c) }
 }
 
 /// Calculates the inverse of a matrix which would look in the direction `look`, using `up` as its up vector
-pub fn mat3_look_at_inverse(look: SPVec3, up: SPVec3) -> SPMat3 {
-    unsafe { spMat3LookAtInverse(look, up) }
+pub fn mat3_look_at_inverse(look: &SPVec3, up: &SPVec3) -> SPMat3 {
+    unsafe { spMat3LookAtInverse(*look, *up) }
 }
 
 /// Rotates matrix `m` `mangle` (degrees|radians) around `axis`, storing the result in `result`
@@ -250,8 +250,8 @@ pub fn mat3_slerp_ptr(a: *mut SPMat3, b: *mut SPMat3, fraction: f64, result: *mu
 }
 
 /// Checks if point `p` is to the left of the line that passes through `a` before it passes through `b`
-pub fn point_is_left_of_line(p: SPVec3, a: SPVec3, b: SPVec3) -> bool {
-    unsafe { spPointIsLeftOfLine(p, a, b) }
+pub fn point_is_left_of_line(p: &SPVec3, a: &SPVec3, b: &SPVec3) -> bool {
+    unsafe { spPointIsLeftOfLine(*p, *a, *b) }
 }
 
 // Smoke tests to ensure I'm calling the correct Sapiens functions
@@ -305,7 +305,7 @@ mod vec2_smoke_tests {
         let a = SPVec2 { x: 2.0, y: 2.0 };
         let b = SPVec2 { x: 3.0, y: 4.0 };
 
-        let result = vec2_add(a, b);
+        let result = vec2_add(&a, &b);
 
         assert_eq!(result, SPVec2 { x: 5.0, y: 6.0 });
     }
@@ -315,7 +315,7 @@ mod vec2_smoke_tests {
         let a = SPVec2 { x: 2.0, y: 2.0 };
         let b = SPVec2 { x: 3.0, y: 4.0 };
 
-        let result = vec2_sub(a, b);
+        let result = vec2_sub(&a, &b);
 
         assert_eq!(result, SPVec2 { x: -1.0, y: -2.0 });
     }
@@ -324,7 +324,7 @@ mod vec2_smoke_tests {
     fn vec2_mul_test() {
         let a = SPVec2 { x: 2.0, y: 3.0 };
 
-        let result = vec2_mul(a, 4.0);
+        let result = vec2_mul(&a, 4.0);
 
         assert_eq!(result, SPVec2 { x: 8.0, y: 12.0 });
     }
@@ -333,7 +333,7 @@ mod vec2_smoke_tests {
     fn vec2_div_test() {
         let a = SPVec2 { x: 2.0, y: 3.0 };
 
-        let result = vec2_div(a, 2.0);
+        let result = vec2_div(&a, 2.0);
 
         assert_eq!(result, SPVec2 { x: 1.0, y: 1.5 })
     }
@@ -342,7 +342,7 @@ mod vec2_smoke_tests {
     fn vec2_neg_test() {
         let a = SPVec2 { x: 2.0, y: 3.0 };
 
-        let result = vec2_neg(a);
+        let result = vec2_neg(&a);
 
         assert_eq!(result, SPVec2 { x: -2.0, y: -3.0 })
     }
@@ -365,7 +365,7 @@ mod vec3_smoke_tests {
             z: 5.0,
         };
 
-        let result = vec3_add(a, b);
+        let result = vec3_add(&a, &b);
 
         assert_eq!(
             result,
@@ -390,7 +390,7 @@ mod vec3_smoke_tests {
             z: 5.0,
         };
 
-        let result = vec3_sub(a, b);
+        let result = vec3_sub(&a, &b);
 
         assert_eq!(
             result,
@@ -410,7 +410,7 @@ mod vec3_smoke_tests {
             z: 5.0,
         };
 
-        let result = vec3_mul(a, 4.0);
+        let result = vec3_mul(&a, 4.0);
 
         assert_eq!(
             result,
@@ -430,7 +430,7 @@ mod vec3_smoke_tests {
             z: 5.0,
         };
 
-        let result = vec3_div(a, 2.0);
+        let result = vec3_div(&a, 2.0);
 
         assert_eq!(
             result,
@@ -450,7 +450,7 @@ mod vec3_smoke_tests {
             z: 5.0,
         };
 
-        let result = vec3_neg(a);
+        let result = vec3_neg(&a);
 
         assert_eq!(
             result,
@@ -482,7 +482,7 @@ mod vec4_smoke_tests {
             w: 6.0,
         };
 
-        let result = vec4_add(a, b);
+        let result = vec4_add(&a, &b);
 
         assert_eq!(
             result,
@@ -510,7 +510,7 @@ mod vec4_smoke_tests {
             w: 6.0,
         };
 
-        let result = vec4_sub(a, b);
+        let result = vec4_sub(&a, &b);
 
         assert_eq!(
             result,
@@ -532,7 +532,7 @@ mod vec4_smoke_tests {
             w: 6.0,
         };
 
-        let result = vec4_mul(a, 4.0);
+        let result = vec4_mul(&a, 4.0);
 
         assert_eq!(
             result,
@@ -554,7 +554,7 @@ mod vec4_smoke_tests {
             w: 6.0,
         };
 
-        let result = vec4_div(a, 2.0);
+        let result = vec4_div(&a, 2.0);
 
         assert_eq!(
             result,
@@ -576,7 +576,7 @@ mod vec4_smoke_tests {
             w: 6.0,
         };
 
-        let result = vec4_neg(a);
+        let result = vec4_neg(&a);
 
         assert_eq!(
             result,
@@ -602,7 +602,7 @@ mod linear_algebra_smoke_tests {
             z: 2.0,
         };
 
-        let normalized_vec = vec3_normalize(vec);
+        let normalized_vec = vec3_normalize(&vec);
 
         assert_eq!(
             normalized_vec,
@@ -627,7 +627,7 @@ mod linear_algebra_smoke_tests {
             z: 0.0,
         };
 
-        let dot = vec3_dot(a, b);
+        let dot = vec3_dot(&a, &b);
 
         assert_eq!(dot, 0.707);
     }
@@ -645,7 +645,7 @@ mod linear_algebra_smoke_tests {
             z: 0.0,
         };
 
-        let cross = vec3_cross(a, b);
+        let cross = vec3_cross(&a, &b);
 
         assert_eq!(
             cross,
@@ -665,7 +665,7 @@ mod linear_algebra_smoke_tests {
             z: 2.0,
         };
 
-        let len = vec3_length(vec);
+        let len = vec3_length(&vec);
 
         assert_eq!(len, 4.0);
     }
@@ -678,7 +678,7 @@ mod linear_algebra_smoke_tests {
             z: 2.0,
         };
 
-        let len = vec3_length2(vec);
+        let len = vec3_length2(&vec);
 
         assert_eq!(len, 16.0);
     }
@@ -696,7 +696,7 @@ mod linear_algebra_smoke_tests {
             z: 1.0,
         };
 
-        let distance = vec3_distance(a, b);
+        let distance = vec3_distance&a, &b);
 
         assert_eq!(distance, 3.0);
     }
@@ -714,7 +714,7 @@ mod linear_algebra_smoke_tests {
             z: 1.0,
         };
 
-        let distance = vec3_distance2(a, b);
+        let distance = vec3_distance2(&a, &b);
 
         assert_eq!(distance, 9.0);
     }
--- tests/particles_api_test.rs
+++ tests/particles_api_test.rs
@@ -14,6 +14,7 @@ use sapiens_rs::sp::particles::*;
 use sapiens_rs::sp_meters_to_prerender;
 use sapiens_rs_macros::export_to_sapiens;
 use sapiens_sys::*;
+use std::ptr::slice_from_raw_parts_mut;
 
 #[derive(Debug, PartialEq, FromPrimitive, ToPrimitive)]
 enum VanillaEmitterType {
@@ -113,19 +114,19 @@ fn emitter_was_added(
         VanillaEmitterType::WoodChop => {
             remove_immediately = true;
 
-            let pos_length = sp::vec3_length(emitter_state.p);
-            let normalized_pos = sp::vec3_div(emitter_state.p, pos_length);
-            let gravity = sp::vec3_mul(normalized_pos, sp_meters_to_prerender!(-10.0));
+            let pos_length = sp::vec3_length(&emitter_state.p);
+            let normalized_pos = sp::vec3_div(&emitter_state.p, pos_length);
+            let gravity = sp::vec3_mul(&normalized_pos, sp_meters_to_prerender!(-10.0));
 
             for _ in 0..32 {
                 let rand_pos_vec =
-                    sp::vec3_mul(thread_state.rand.get_vec3(), sp_meters_to_prerender!(0.2));
+                    sp::vec3_mul(&thread_state.rand.get_vec3(), sp_meters_to_prerender!(0.2));
                 let rand_vel_vec = thread_state.rand.get_vec3();
 
                 let mut state = ParticleState {
-                    p: sp::vec3_add(sp::vec3_mul(normalized_pos, pos_length), rand_pos_vec),
+                    p: sp::vec3_add(&sp::vec3_mul(&normalized_pos, pos_length), &rand_pos_vec),
                     v: sp::vec3_mul(
-                        sp::vec3_add(normalized_pos, rand_vel_vec),
+                        &sp::vec3_add(&normalized_pos, &rand_vel_vec),
                         sp_meters_to_prerender!(1.0),
                     ),
                     gravity,
@@ -142,22 +143,22 @@ fn emitter_was_added(
         }
         VanillaEmitterType::Feathers => {
             remove_immediately = true;
-            let pos_length = sp::vec3_length(emitter_state.p);
-            let normalized_pos = sp::vec3_div(emitter_state.p, pos_length);
-            let gravity = sp::vec3_mul(normalized_pos, sp_meters_to_prerender!(-2.0));
+            let pos_length = sp::vec3_length(&emitter_state.p);
+            let normalized_pos = sp::vec3_div(&emitter_state.p, pos_length);
+            let gravity = sp::vec3_mul(&normalized_pos, sp_meters_to_prerender!(-2.0));
 
             for _ in 0..32 {
                 let rand_pos_vec =
-                    sp::vec3_mul(thread_state.rand.get_vec3(), sp_meters_to_prerender!(0.2));
+                    sp::vec3_mul(&thread_state.rand.get_vec3(), sp_meters_to_prerender!(0.2));
                 let rand_vel_vec = thread_state.rand.get_vec3();
 
                 let mut state = ParticleState {
                     p: sp::vec3_add(
-                        sp::vec3_mul(normalized_pos, pos_length + sp_meters_to_prerender!(0.25)),
-                        rand_pos_vec,
+                        &sp::vec3_mul(&normalized_pos, pos_length + sp_meters_to_prerender!(0.25)),
+                        &rand_pos_vec,
                     ),
                     v: sp::vec3_mul(
-                        sp::vec3_add(normalized_pos, rand_vel_vec),
+                        &sp::vec3_add(&normalized_pos, &rand_vel_vec),
                         sp_meters_to_prerender!(1.0),
                     ),
                     gravity,
@@ -207,8 +208,8 @@ fn update_emitter(
             VanillaEmitterType::Campfire => {
                 // smoke
                 if emitter_state.counters[0] == 0 {
-                    let pos_length = sp::vec3_length(emitter_state.p);
-                    let normalized_pos = sp::vec3_div(emitter_state.p, pos_length);
+                    let pos_length = sp::vec3_length(&emitter_state.p);
+                    let normalized_pos = sp::vec3_div(&emitter_state.p, pos_length);
 
                     let lookup = SPVec3 {
                         x: (normalized_pos.x + 1.2) * 99999.9,
@@ -231,14 +232,14 @@ fn update_emitter(
                     let noise_value_c = thread_state.noise.get(&lookup_c, 2);
 
                     let intermediate_gravity = sp::vec3_mul(
-                        sp::mat3_get_row(emitter_state.rot, 0),
+                        &sp::mat3_get_row(&emitter_state.rot, 0),
                         sp_meters_to_prerender!(noise_value) * 0.5,
                     );
 
                     let mut state = SPParticleState {
-                        p: sp::vec3_mul(normalized_pos, pos_length + sp_meters_to_prerender!(0.2)),
+                        p: sp::vec3_mul(&normalized_pos, pos_length + sp_meters_to_prerender!(0.2)),
                         v: sp::vec3_mul(
-                            normalized_pos,
+                            &normalized_pos,
                             sp_meters_to_prerender!(0.5 + noise_value_c) * 0.5,
                         ),
                         particleTextureType: 2,
@@ -247,9 +248,9 @@ fn update_emitter(
                         randomValueA: rand.get_float(),
                         randomValueB: 0.0,
                         gravity: sp::vec3_add(
-                            intermediate_gravity,
-                            sp::vec3_mul(
-                                sp::mat3_get_row(emitter_state.rot, 2),
+                            &intermediate_gravity,
+                            &sp::vec3_mul(
+                                &sp::mat3_get_row(&emitter_state.rot, 2),
                                 sp_meters_to_prerender!(noise_value_b) * 0.5,
                             ),
                         ),
@@ -265,7 +266,8 @@ fn update_emitter(
 
                 // Flame 1
                 if emitter_state.counters[1] == 0 {
-                    let rand_pos_vec = sp::vec3_mul(rand.get_vec3(), sp_meters_to_prerender!(0.04));
+                    let rand_pos_vec =
+                        sp::vec3_mul(&rand.get_vec3(), sp_meters_to_prerender!(0.04));
                     let scale_average = 0.5;
 
                     emit_fire_particle(thread_state, emitter_state, scale_average, &rand_pos_vec);
@@ -277,11 +279,12 @@ fn update_emitter(
 
                 // Flame 2
                 if emitter_state.counters[2] == 0 {
-                    let rand_pos_vec = sp::vec3_mul(rand.get_vec3(), sp_meters_to_prerender!(0.04));
+                    let rand_pos_vec =
+                        sp::vec3_mul(&rand.get_vec3(), sp_meters_to_prerender!(0.04));
                     let rand_pos_vec = sp::vec3_add(
-                        rand_pos_vec,
-                        sp::vec3_mul(
-                            sp::mat3_get_row(emitter_state.rot, 0),
+                        &rand_pos_vec,
+                        &sp::vec3_mul(
+                            &sp::mat3_get_row(&emitter_state.rot, 0),
                             sp_meters_to_prerender!(0.2),
                         ),
                     );
@@ -297,11 +300,12 @@ fn update_emitter(
 
                 // Flame 3
                 if emitter_state.counters[4] == 0 {
-                    let rand_pos_vec = sp::vec3_mul(rand.get_vec3(), sp_meters_to_prerender!(0.04));
+                    let rand_pos_vec =
+                        sp::vec3_mul(&rand.get_vec3(), sp_meters_to_prerender!(0.04));
                     let rand_pos_vec = sp::vec3_add(
-                        rand_pos_vec,
-                        sp::vec3_mul(
-                            sp::mat3_get_row(emitter_state.rot, 2),
+                        &rand_pos_vec,
+                        &sp::vec3_mul(
+                            &sp::mat3_get_row(&emitter_state.rot, 2),
                             sp_meters_to_prerender!(0.1),
                         ),
                     );
@@ -313,26 +317,26 @@ fn update_emitter(
                 } else {
                     if emitter_state.counters[3] == 18 {
                         // Spark
-                        let pos_length = sp::vec3_length(emitter_state.p);
-                        let normalized_pos = sp::vec3_div(emitter_state.p, pos_length);
+                        let pos_length = sp::vec3_length(&emitter_state.p);
+                        let normalized_pos = sp::vec3_div(&emitter_state.p, pos_length);
 
                         let rand_vec = rand.get_vec3();
-                        let rand_pos_vec = sp::vec3_mul(rand_vec, sp_meters_to_prerender!(0.1));
-                        let rand_vel_vec = sp::vec3_mul(rand_vec, sp_meters_to_prerender!(0.4));
+                        let rand_pos_vec = sp::vec3_mul(&rand_vec, sp_meters_to_prerender!(0.1));
+                        let rand_vel_vec = sp::vec3_mul(&rand_vec, sp_meters_to_prerender!(0.4));
 
                         let mut state = SPParticleState {
                             p: sp::vec3_add(
-                                sp::vec3_mul(
-                                    normalized_pos,
+                                &sp::vec3_mul(
+                                    &normalized_pos,
                                     pos_length + sp_meters_to_prerender!(0.1),
                                 ),
-                                rand_pos_vec,
+                                &rand_pos_vec,
                             ),
                             v: sp::vec3_mul(
-                                sp::vec3_add(normalized_pos, rand_vel_vec),
+                                &sp::vec3_add(&normalized_pos, &rand_vel_vec),
                                 sp_meters_to_prerender!(2.0 + rand.get_float() * 0.5),
                             ),
-                            gravity: sp::vec3_mul(rand.get_vec3(), sp_meters_to_prerender!(1.0)),
+                            gravity: sp::vec3_mul(&rand.get_vec3(), sp_meters_to_prerender!(1.0)),
                             lifeLeft: 1.0,
                             scale: 0.01 + rand.get_float() * 0.02,
                             randomValueA: rand.get_float(),
@@ -359,10 +363,37 @@ fn update_emitter(
 
 fn emit_fire_particle(
     thread_state: &ThreadState,
-    emitter_state: &EmitterState,
+    emitter_state: &mut EmitterState,
     scale_average: f64,
     rand_pos_vec: &SPVec3,
 ) {
+    let rand = &thread_state.rand;
+
+    let pos_length = sp::vec3_length(&emitter_state.p);
+    let normalized_pos = sp::vec3_div(&emitter_state.p, pos_length);
+
+    let mut state = SPParticleState {
+        p: sp::vec3_add(
+            &sp::vec3_mul(&normalized_pos, pos_length + sp_meters_to_prerender!(0.1)),
+            rand_pos_vec,
+        ),
+        v: sp::vec3_mul(
+            &normalized_pos,
+            sp_meters_to_prerender!(0.2 + rand.get_float() * 0.2),
+        ),
+        gravity: Default::default(),
+        lifeLeft: 1.0,
+        scale: scale_average + rand.get_float() * 0.2,
+        randomValueA: rand.get_float(),
+        randomValueB: rand.get_float(),
+        userData: Default::default(),
+        particleTextureType: match rand.get_float() {
+            x if x < 0.5 => 1,
+            _ => 4,
+        },
+    };
+
+    thread_state.add_particle(emitter_state, VanillaRenderType::Fire, &mut state);
 }
 
 #[cfg(test)]
@@ -372,7 +403,7 @@ mod tests {
     #[test]
     fn test_emitter_types_count() {
         let emitter_types_count = get_emitter_types_count();
-        let ffi_emitter_types_count = spGetEmitterTypesCount();
+        let ffi_emitter_types_count = unsafe { spGetEmitterTypesCount() };
 
         assert_eq!(emitter_types_count, ffi_emitter_types_count);
     }
@@ -380,7 +411,7 @@ mod tests {
     #[test]
     fn test_get_emitter_types() {
         let emitter_types_count = get_emitter_types_count() as usize;
-        let raw_ffi_emitters = spGetEmitterTypes();
+        let raw_ffi_emitters = unsafe { spGetEmitterTypes() };
 
         let ffi_emitters = unsafe {
             Vec::<SPParticleEmitterTypeInfo>::from_raw_parts(
@@ -398,7 +429,7 @@ mod tests {
     #[test]
     fn test_render_group_types_count() {
         let render_group_types_count = get_render_group_types_count();
-        let ffi_render_group_types_count = spGetRenderGroupTypesCount() as u32;
+        let ffi_render_group_types_count = unsafe { spGetRenderGroupTypesCount() } as u32;
 
         assert_eq!(render_group_types_count, ffi_render_group_types_count);
     }
@@ -406,7 +437,7 @@ mod tests {
     #[test]
     fn test_get_render_group_types() {
         let render_group_types_count = get_render_group_types_count() as usize;
-        let raw_ffi_render_types = spGetRenderGroupTypes();
+        let raw_ffi_render_types = unsafe { spGetRenderGroupTypes() };
 
         let ffi_render_types = unsafe {
             Vec::<SPParticleRenderGroupInfo>::from_raw_parts(
